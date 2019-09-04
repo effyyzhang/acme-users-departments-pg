06a2ab2e3fb972825f098dfcec8e3a6e3baff9d6
Effy Zhang <effy@squareup.com>
2019-09-03 21:43:29 -0400

    add changes

---
+++ db.js
@@ -0,0 +1,63 @@
+const pg = require('pg');
+const {Client} = pg;
+const client = new Client ('postgres://localhost/users_db')
+const faker = require('faker')
+const uuid = require('uuid')
+client.connect();
+
+const generateIds = (...names) => {
+    return names.reduce((acc, name) => {
+        acc[name] = uuid.v4()
+        return acc;
+    }, {})
+}
+
+const ids = generateIds('Dep1_id','Dep2_id','Dep3_id','Dep4_id','User1_id','User2_id','User3_id','User4_id')
+const Dep1_Name = faker.commerce.department()
+const Dep2_Name = faker.commerce.department()
+const Dep3_Name = faker.commerce.department()
+const Dep4_Name = faker.commerce.department()
+const User1_Name = faker.name.firstName()
+const User2_Name = faker.name.firstName()
+const User3_Name = faker.name.firstName()
+const User4_Name = faker.name.firstName()
+
+console.log(ids.Dep1_id)
+const SQL = `
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments(
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE users(
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) UNIQUE,
+    department_id UUID REFERENCES departments(id) NULL
+);
+
+INSERT INTO departments(id, name) VALUES ('${ids.Dep1_id}', '${Dep1_Name}');
+INSERT INTO departments(id, name) VALUES ('${ids.Dep2_id}', '${Dep2_Name}');
+INSERT INTO departments(id, name) VALUES ('${ids.Dep3_id}', '${Dep3_Name}');
+INSERT INTO departments(id, name) VALUES ('${ids.Dep4_id}', '${Dep4_Name}');
+
+INSERT INTO users(id, name, department_id) VALUES ('${ids.User1_id}', '${User1_Name}','${ids.Dep1_id}');
+INSERT INTO users(id, name, department_id) VALUES ('${ids.User2_id}', '${User2_Name}','${ids.Dep3_id}');
+INSERT INTO users(id, name, department_id) VALUES ('${ids.User3_id}', '${User3_Name}','${ids.Dep2_id}');
+INSERT INTO users(id, name) VALUES ('${ids.User4_id}', '${User4_Name}');
+
+SELECT departments.id as department_id
+
+
+
+`
+
+const sync = async() => {
+    await client.query(SQL)
+    console.log('success')
+}
+
+sync()
+.catch(ex => console.log(ex));
